6fd6f0e9780649bb38e13f048166fa77ab3c429e
tcardy <thomasb.cardy@gmail.com>
2017-10-30 09:15:45 -0400

    Added Bootstrap
    Changed the button layout to a bootstrap Navbar

--- project2/src/app/app.module.ts
+++ project2/src/app/app.module.ts
@@ -4,18 +4,21 @@ import { SpotifyModule } from './services/spotify.module';
 import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { PlaylistComponent } from './playlist/playlist.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'playlist', component: PlaylistComponent}
+  {path: 'playlist', component: PlaylistComponent},
+  {path: 'about', component: AboutComponent}
 ];
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PlaylistComponent
+    PlaylistComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule, SpotifyModule, RouterModule.forRoot(routes)
